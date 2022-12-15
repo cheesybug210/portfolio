066d3bd8652ea89c1b02489338ea589f1aad831d
cheesybug210 <udayveer_sodhi+cs1300@brown.edu>
2022-12-14 23:34:48 -0500

    responsive redesign

--- src/components/ResponsiveRedesign.jsx
+++ src/components/ResponsiveRedesign.jsx
@@ -1,5 +1,5 @@
 import ProjectTop from "./ProjectTop";
-import { Col, Container, Row, Image } from "react-bootstrap";
+import { Col, Container, Row, Image, Carousel } from "react-bootstrap";
 import "./Project.css";
 
 import w1 from '../assets/images/responsive-redesign/Website - 1.png';
@@ -36,57 +36,213 @@ export default function ResponsiveRedesign (props) {
 
         <Row className="section">
             <h2 className="montserrat">Overview</h2>
-            <div className="open-sans">Wah</div>
+            <div className="open-sans">The Pawtucket government website is a good example of website that, while responsive, has several usability, learnability, memorability, and accessibility issues. Since it is a government website and people just want to get information quickly, it wasn't able to serve its purpose effectively. Here, I analyse the problems with the website and put forward a potential redesign with a live demo.</div>
         </Row>
 
         <Row className="section">
             <Col>
-                <h2 className="montserrat">Sketch</h2>
-                <Image fluid="true" src={w1} />
+                <h2 className="montserrat">Current Website</h2>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img src={w1} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w2} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w3} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w4} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w5} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w6} className="w-100" alt="..." />
+                    </Carousel.Item>
+                </Carousel>
+                <div className="open-sans">
+                <h6 className="montserrat text-muted mt-3 mb-0">Website</h6>
+                <a href="https://pawtucketri.com/">https://pawtucketri.com/</a>
+
+                </div>
+            </Col>
+            {/* 
+            <Col>
+                <h2 className="montserrat">Redesigned Website</h2>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img src={w1} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w2} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w3} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w4} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w5} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={w6} className="w-100" alt="..." />
+                    </Carousel.Item>
+                </Carousel>
+                <div className="open-sans">
+                    <h6 className="montserrat text-muted mt-3 mb-0">Website</h6>
+                    <a href="https://pawtucketri.com/">https://pawtucketri.com/</a>
+
+                </div>
+            </Col> */}
+        </Row>
+        <Row className="section">
+            <h2 className="montserrat">Problems</h2>
+            <Col>
+                <h4 className="montserrat">Usability</h4>
+                <div className="open-sans">
+                    <ul className="list">
+                        <li>The "Follow us on ..." social media banner has no links to any social media, which are also not
+                            present anywhere else on the page.</li>
+                        <li>The carousel has no signs to indicate that it is a carousel (like arrows or the selector at the
+                            bottom). It initally just appears as an image and then fades in and out, requiring to be swiped to
+                            change what's displayed (an action also not indicated).</li>
+                        <li>Different fonts are used throughout the website with no consistent design language for links and
+                            other key elements.</li>
+                        <li>The "Connect With Us" logo is not clearly indicated as something that must be pressed, and
+                            without any caption or obviously identifiable symbol, the user is required to make a greater
+                            effort to read the text and understand what it is for.</li>
+                        <li>The link to view more news is located a long way above the actual news section, which is
+                            confusing.</li>
+                        <li>There are no headings used anywhere, which inhibits the effort to produce a clear visual
+                            hierarchy.</li>
+                        <li>Font sizes do not adjust on resizing, making it difficult to read on smaller screens.</li>
+                        <li>Efficiency is a problem:
+                            <ul className="list-unstyled list-line mb-3">
+                                <li>Something simple and commonly used like "Report a problem" requires scrolling all the way to
+                                    the bottom of the page to do.</li>
+                                <li>Other frequent actions (like "Email Log In") are also present at the bottom, increasing the
+                                    time required to do these actions.</li>
+                                <li>Important information seems to be displayed on the carousel, which we've seen is confusing
+                                    to figure out how to use.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </Col>
             <Col>
-                <h2 className="montserrat">Description</h2>
+                <h4 className="montserrat">Learnability</h4>
                 <div className="open-sans">
-
-                    The problem the microwave is trying to solve is that of heating food or beverages. It has several components, which function as follows.
-
-                    <ul>
-                        <li>Microwave door with see-through glass: opens to allow the item to be put inside and is see-through to allow the user to view the item being heated.</li>
-                        <li>Time / weight display: displays the current time, time remaining to heat, or weight selected to heat.</li>
-                        <li>Preset buttons: buttons with built-in functionality, such as a popcorn timer.</li>
-                        <li>Time / weight knob: adjust the time / weight</li>
-                        <li>Preset buttons: a row of buttons with built-in functionality, such as a Quick Min button</li>
-                        <li>Action buttons: buttons to stop or start the microwave</li>
-                        <li>Door release button: opens the microwave door</li>
+                    <ul className="list">
+                        <li>Some items may take a little time to get used to, like the carousel at the beginning</li>
+                        <li>Some commonly used items (like "Report a Problem") may not be in a location people expect due to
+                            their popularity, but for specific tasks, it should be easy to remember where they are.</li>
+                        <li>Finding related tasks may be tricky - due to the lack of clear hierarchy, there may be some
+                            issues with navigation or figuring out where things are.</li>
+                    </ul>
+                </div>
+            </Col>
+            </Row>
+        <Row className="section">
+            <Col>
+                <h4 className="montserrat">Memorability</h4>
+                <div className="open-sans">
+                    <ul className="list">
+                        <li>Some things (though inefficient) are easy to remember: e.g. it would be easy to remember to
+                            scroll to the bottom each time you needed to report a problem..</li>
+                        <li>Other things are harder to remember: e.g. a user may not remember that they need to scroll
+                            through the carousel to find Covid (or other) information</li>
+                        <li>Some things may be memorable but may move around on the page: e.g. the new City Hall business
+                            hours are on the carousel for now, but may not always be.</li>
+                    </ul>
+                </div>
+            </Col>
+            <Col>
+                <h4 className="montserrat">Accessibility</h4>
+                <div className="open-sans">
+                    <ul className="list">
+                        <li>The most common accessibility issue is contrast: WebAIM WAVE detected 46 "Very low contrast
+                            errors". This is because the website often pairs light font colours (like a light orangish-red)
+                            with light background colours (like light grey), making it difficult to read parts of the page.
+                        </li>
+                        <li>There are also issues with empty headers or empty links. While there are invisible and may not
+                            impact an able user, this might be confusing for a user who requires a screen reader to navigate
+                            websites.</li>
+                        <li>Some of the empty links reduce usability for everyone: empty links like Twitter and Facebook
+                            should be visible</li>
+                        <li>There are many redundant link alerts where adjacent links go to the same URL. This may cause
+                            some minor confusion for able users and may be much worse for users requiring screen readers,
+                            which will repeat the same link many times.</li>
                     </ul>
                 </div>
             </Col>
         </Row>
         <Row className="section">
-            <h2 className="montserrat">Observations</h2>
-        </Row>
-        <Row className="section">
+            <h2 className="montserrat">Redesign</h2>
             <Col>
-                <h2 className="montserrat">Questions</h2>
-                <div className="open-sans">
-                    Wah
-                </div>
+            <h4 className="montserrat">Visual design style guide</h4>
+            <Image fluid src={vd} />
             </Col>
             <Col>
-                <h2 className="montserrat">Responses</h2>
+                <h4 className="montserrat">Explanation</h4>
                 <div className="open-sans">
-                    Wah
+                    Something about the visual design style guide
                 </div>
             </Col>
         </Row>
         <Row className="section">
-            <h2 className="montserrat">Personas</h2>
-            <div className="open-sans">Wah</div>
+            <h2 className="montserrat">Lofi Prototypes</h2>
+            <Col>
+                <h4 className="montserrat">Mobile</h4>
+                <Image className="zoom-hover" fluid src={lfm} />
+            </Col>
+            <Col>
+                <h4 className="montserrat">Tablet</h4>
+                <Image className="zoom-hover" fluid src={lft} />
+            </Col>
+            <Col>
+                <h4 className="montserrat">Desktop</h4>
+                <Image className="zoom-hover" fluid src={lfd} />
+            </Col>
         </Row>
         <Row className="section">
-            <h2 className="montserrat">Storyboard</h2>
-            <div className="open-sans">Wah</div>
+            <h2 className="montserrat">Hifi Prototypes</h2>
+            <Col>
+                <h4 className="montserrat">Mobile</h4>
+                <Image className="zoom-hover" fluid src={hfm} />
+            </Col>
+            <Col>
+                <h4 className="montserrat">Tablet</h4>
+                <Image className="zoom-hover" fluid src={hft} />
+            </Col>
+            <Col>
+                <h4 className="montserrat">Desktop</h4>
+                <Image className="zoom-hover" fluid src={hfd} />
+            </Col>
         </Row>
+        <Row className="section">
+            <Col>
+                <h2 className="montserrat">Redesigned Website</h2>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img src={n1} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={n2} className="w-100" alt="..." />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={n3} className="w-100" alt="..." />
+                    </Carousel.Item>
+                </Carousel>
+                <div className="open-sans">
+                    <h6 className="montserrat text-muted mt-3 mb-0">Website</h6>
+                    <a href="https://cheesybug210.github.io/responsive-redesign-website">https://cheesybug210.github.io/responsive-redesign-website</a>
+
+                </div>
+            </Col>
+            </Row>
     </Container>;
 
 }
